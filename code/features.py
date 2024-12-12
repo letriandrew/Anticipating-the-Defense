0ed6902057b7fc4df87935eb6dfc1dba42dbc19d
Andrew.Le <letriandrew@gmail.com>
2024-12-12 15:02:45 -0800

    check normalization

--- code/features.py
+++ code/features.py
@@ -27,7 +27,7 @@ def normalize(dataframe):
     dataframe[numerical_categories] = scaler.fit_transform(dataframe[numerical_categories])
 
     #one-hot encode categorical features (binary/categorical columns)
-    onehot_encoder = OneHotEncoder(sparse=False, drop='first')  # drop='first' avoids multicollinearity
+    onehot_encoder = OneHotEncoder(sparse_output=False, drop='first')  # drop='first' avoids multicollinearity
     onehot_encoded = onehot_encoder.fit_transform(dataframe[categorical_categories_onehot])
     onehot_encoded_df = pd.DataFrame(onehot_encoded, columns=onehot_encoder.get_feature_names_out(categorical_categories_onehot))
     dataframe = dataframe.drop(columns=categorical_categories_onehot)  # drop the original columns
@@ -42,16 +42,19 @@ def normalize(dataframe):
 
 
 def create_context():
-    for week in range(1, 9):
+    for week in range(1, 2):
         print(f"Creating {week} Context/Categorical Features")
         df = pd.read_csv(f'data/processed/final_tracking_week_{week}.csv')
 
         df = normalize(df)
 
-        #df.to_csv(f'data/processed/final_tracking_week_{week}_normalized.csv', index=False)
+        df.to_csv(f'data/processed/final_tracking_week_{week}_normalized.csv', index=False)
         print(f"Week {week} processed and saved.")
 
     
 
 ## MAIN #########################################################################################
 #################################################################################################
+
+if __name__ == "__main__":
+    create_context()
